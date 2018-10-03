9320d816b237a17af97b857f12cb0a708e2189e9
tkromer <7s3UZ5Fp9kFH>
2018-10-03 18:42:59 -0400

    First commit

---
+++ src/discgolftracker/Course.java
@@ -0,0 +1,199 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discgolftracker;
+
+import java.io.Serializable;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Jim
+ */
+public class Course implements Serializable{
+    private String courseName;
+    private String courseLocation;
+    private int courseLength;
+    private int courseAvg;
+    private int courseBest;
+    //Information about the holes is stored in an array of hole objects
+     hole[] holeInfo = new hole[18];
+    
+    public Course() {
+        this.setCourseAvg(0);
+        this.setCourseBest(0);
+         
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseLocation() {
+        return courseLocation;
+    }
+
+    public int getCourseLength() {
+        return courseLength;
+    }
+
+    public int getCourseAvg() {
+        return courseAvg;
+    }
+
+    public int getCourseBest() {
+        return courseBest;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setCourseLocation(String courseLocation) {
+        this.courseLocation = courseLocation;
+    }
+
+    public void setCourseLength(int courseLength) {
+        this.courseLength = courseLength;
+    }
+
+    public void setCourseAvg(int courseAvg) {
+        this.courseAvg = courseAvg;
+    }
+
+    public void setCourseBest(int courseBest) {
+        this.courseBest = courseBest;
+    }
+    
+    public void setCourse() {
+        int usrPar;
+        int usrDist;
+        Scanner in = new Scanner(System.in);
+        String input;
+        hole test;
+        
+        System.out.print("Name of the course: ");
+        input = in.nextLine();
+        this.setCourseName(input);
+        System.out.print("\nNumber of holes: ");
+        input = in.nextLine();
+        this.setCourseLength(Integer.parseInt(input));
+        
+        for (int i = 0; i < this.getCourseLength(); i++){
+            
+            // Get course info from user
+            System.out.print("Hole " + (i+1) + " par: ");
+            usrPar = Integer.parseInt(in.next());            
+            System.out.print("Hole " + (i+1) + " distance in ft: ");
+            usrDist = Integer.parseInt(in.next());
+            
+            test = new hole(usrPar, usrDist);
+            System.out.println(i);
+            holeInfo[i] = new hole(usrPar, usrDist);            
+            
+            System.out.println("Hole " + (i+1) + " saved successfully!\n");
+        }
+        
+        in.close();
+    }
+    
+    public void printCourse() {
+        int count;
+        int i;
+        
+        this.displayName();
+        
+        //Print Column Headers
+        System.out.println("| Hole #  | Par  | Distance |");
+        
+        System.out.print("|");
+        for (count = 0; count < 27; count++) {
+            System.out.print("-");
+        }
+        
+        System.out.print("|\n");
+        
+        for (count = 0; count < this.getCourseLength(); count++) {
+            System.out.print("|   ");
+            //Print Hole Number
+            if ((count + 1) < 10) {
+                System.out.print(" " + (count + 1) + "    |");
+            }
+            else {
+                System.out.print((count + 1) + "    |");
+            }
+            
+            // Print par
+            if (holeInfo[count].getPar() < 10) {
+                System.out.print("  " + holeInfo[count].getPar() + "   |");
+            }
+            else {
+                System.out.print(" " + holeInfo[count].getPar() + "   |");
+            }
+            
+            // Print distance
+            if (holeInfo[count].getDistance() < 999) {
+                System.out.print("   " + holeInfo[count].getDistance() + "ft  |");
+            }
+            else {
+                System.out.print("  " + holeInfo[count].getDistance() + "ft  |");
+            }
+
+            System.out.print("\n|");
+            for (i = 0; i < 27; i++) {
+                System.out.print("-");
+            }
+            System.out.print("|\n");
+        }
+        
+    }
+    
+    public void displayName() {
+        int count;
+        int spacing = 27 - this.getCourseName().length();
+        
+        // Prints formatted course name
+        System.out.print("|");
+        for (count = 0; count < 27; count++) {
+            System.out.print("-");
+        }
+        System.out.print("|\n");
+        
+        System.out.print("|");
+        
+        if(spacing % 2 == 0) {
+            for (count = 0; count < spacing / 2; count++) {
+                System.out.print(" ");
+            }
+            System.out.print(this.getCourseName());
+            for (count = 0; count < spacing / 2; count++) {
+                System.out.print(" ");
+            }
+        }
+        
+        else {
+            for (count = 0; count < spacing / 2; count++) {
+                System.out.print(" ");
+            }
+            System.out.print(this.getCourseName());
+            for (count = 0; count <= spacing / 2; count++) {
+                System.out.print(" ");
+            }
+        }
+        System.out.print("|\n");
+        
+        System.out.print("|");
+        for (count = 0; count < 27; count++) {
+            System.out.print("-");
+        }
+        System.out.print("|\n");
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/discgolftracker/hole.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discgolftracker;
+
+/**
+ *
+ * @author Jim
+ */
+public class hole {
+    private int par;
+    private int distance;
+    private int best;
+    
+    public hole(int holePar, int holeDistance){
+        this.setPar(holePar);
+        this.setDistance(holeDistance);
+    }
+    
+    public hole() {
+        this.setPar(0);
+        this.setDistance(0);
+    }
+
+    public void setPar(int par) {
+        this.par = par;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getPar() {
+        return par;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+    
+    
+    
+}
